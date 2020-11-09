043dd8b8777ebb377fc2dee4181bfc85b9210165
DarkCaster <dark.caster@outlook.com>
2020-11-09 00:00:00 +0000

    Added InterfaceConfig class - immutable storage for describing interface configuration

---
+++ Src/InterfaceConfig.cpp
@@ -0,0 +1,47 @@
+#include "InterfaceConfig.h"
+
+InterfaceConfig::InterfaceConfig(const bool _isUp, const bool _isPtP, const std::set<IPAddress> &_localIPs, const std::set<IPAddress> &_remoteIPs):
+    isUp(_isUp),
+    isPtP(_isPtP),
+    localIPs(_localIPs),
+    remoteIPs(_remoteIPs)
+{
+}
+
+InterfaceConfig InterfaceConfig::AddLocalIP(const IPAddress& ip) const
+{
+    auto tmpSet(localIPs);
+    tmpSet.insert(ip);
+    return InterfaceConfig(isUp,isPtP,tmpSet,remoteIPs);
+}
+
+InterfaceConfig InterfaceConfig::DelLocalIP(const IPAddress& ip) const
+{
+    auto tmpSet(localIPs);
+    tmpSet.erase(ip);
+    return InterfaceConfig(isUp,isPtP,tmpSet,remoteIPs);
+}
+
+InterfaceConfig InterfaceConfig::AddRemoteIP(const IPAddress& ip) const
+{
+    auto tmpSet(remoteIPs);
+    tmpSet.insert(ip);
+    return InterfaceConfig(isUp,isPtP,localIPs,tmpSet);
+}
+
+InterfaceConfig InterfaceConfig::DelRemoteIP(const IPAddress& ip) const
+{
+    auto tmpSet(remoteIPs);
+    tmpSet.erase(ip);
+    return InterfaceConfig(isUp,isPtP,localIPs,tmpSet);
+}
+
+InterfaceConfig InterfaceConfig::SetState(const bool _isUp) const
+{
+    return InterfaceConfig(_isUp,isPtP,localIPs,remoteIPs);
+}
+
+InterfaceConfig InterfaceConfig::SetType(const bool _isPtP) const
+{
+    return InterfaceConfig(isUp,_isPtP,localIPs,remoteIPs);
+}
---
+++ Src/InterfaceConfig.h
@@ -0,0 +1,24 @@
+#ifndef INTERFACECONFIG_H
+#define INTERFACECONFIG_H
+
+#include "IPAddress.h"
+#include <set>
+
+class InterfaceConfig
+{
+    public:
+        InterfaceConfig(const bool isUp, const bool isPtP, const std::set<IPAddress> &localIPs, const std::set<IPAddress> &remoteIPs);
+        const bool isUp;
+        const bool isPtP;
+        const std::set<IPAddress> localIPs;
+        const std::set<IPAddress> remoteIPs;
+
+        InterfaceConfig AddLocalIP(const IPAddress &ip) const;
+        InterfaceConfig DelLocalIP(const IPAddress &ip) const;
+        InterfaceConfig AddRemoteIP(const IPAddress &ip) const;
+        InterfaceConfig DelRemoteIP(const IPAddress &ip) const;
+        InterfaceConfig SetState(const bool isUp) const;
+        InterfaceConfig SetType(const bool isPtP) const;
+};
+
+#endif // INTERFACECONFIG_H
