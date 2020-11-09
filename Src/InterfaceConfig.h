bf65ffe208901024b48ee7bf7a4505bc8ff2318f
DarkCaster <dark.caster@outlook.com>
2020-11-09 00:00:00 +0000

    InterfaceConfig class updated - added some notes

--- Src/InterfaceConfig.h
+++ Src/InterfaceConfig.h
@@ -23,6 +23,13 @@ class InterfaceConfig
         InterfaceConfig SetState(const bool isUp) const;
         InterfaceConfig SetType(const bool isPtP) const;
 
+        //TODO: add helper methods for checking current network-config sutable for adding non-blackhole routes:
+        //for PtP interfaces - network is sutable for routes management if isUp==true in any case
+        //for non PtP interfaces - network is sutable for routes management if we have non link-local IP addresses assigned to it
+
+        //DO NOT ATTEMPT TO PERFORM ANY PENDING-ROUTES MANAGEMENT IF CURRENT NETWORK CONFIG IS NOT SUTABLE FOR ROUTES MANAGEMENT
+        //blackhole-routes can be added in any state even if isUp==false
+
         friend std::ostream& operator<<(std::ostream& stream, const InterfaceConfig& target);
 };
 
