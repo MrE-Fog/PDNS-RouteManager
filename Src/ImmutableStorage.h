a815d233b4b149f04971347e8ff50f677b1c1df6
DarkCaster <dark.caster@outlook.com>
2020-11-09 00:00:00 +0000

    ImmutableStorage class updated

--- Src/ImmutableStorage.h
+++ Src/ImmutableStorage.h
@@ -8,9 +8,11 @@ template <class T> class ImmutableStorage
     private:
         std::vector<T> storage;
     public:
-        ImmutableStorage(const T &payload) {storage.push_back(payload);}
-        T Get() const { return storage.at(0); }
-        void Set(const T &payload) { storage.clear(); storage.push_back(payload); }
+        bool isUpdated;
+        ImmutableStorage(const T &payload) {storage.push_back(payload);isUpdated=false;}
+        T Get() const {return storage.at(0);}
+        void Set(const T &payload) {storage.clear();storage.push_back(payload);isUpdated=true;}
+
         ImmutableStorage<T>& operator=(const T &payload) {Set(payload); return *this;}
 };
 
