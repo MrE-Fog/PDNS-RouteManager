79500defa9e1179a782544abcd71e64ade05610f
DarkCaster <dark.caster@outlook.com>
2020-11-15 06:13:34 +0300

    ImmutableStorage: added support for getting previously stored element for tracking changes

--- Src/ImmutableStorage.h
+++ Src/ImmutableStorage.h
@@ -1,17 +1,18 @@
 #ifndef IMMUTABLESTORAGE_H
 #define IMMUTABLESTORAGE_H
 
-#include <vector>
+#include <queue>
 
 template <class T> class ImmutableStorage
 {
     private:
-        std::vector<T> storage;
+        std::queue<T> storage;
     public:
         bool isUpdated;
-        ImmutableStorage(const T &payload) {storage.push_back(payload);isUpdated=false;}
-        T Get() const {return storage.at(0);}
-        void Set(const T &payload) {storage.clear();storage.push_back(payload);isUpdated=true;}
+        ImmutableStorage(const T &payload) {storage.push(payload);storage.push(payload);isUpdated=false;}
+        T Get() const {return storage.back();}
+        T Prev() const {return storage.front();}
+        void Set(const T &payload) {storage.pop();storage.push(payload);isUpdated=true;}
 
         ImmutableStorage<T>& operator=(const T &payload) {Set(payload); return *this;}
 };
